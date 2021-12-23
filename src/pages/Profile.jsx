0fe75066b66dbcb5e2d399e59125c0d18d02744a
Sebastian Sierra <sebastian.sierrajc@gmail.com>
2021-12-23 17:45:23 -0500

    cambio para probar

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Profile = () => {
     return (
         <div>
-            <div>porfile</div>
+            <div>
+                <h1>El profile</h1>
+            </div>
             <div>profile</div>
         </div>
     )
